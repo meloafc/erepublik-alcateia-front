765bb51e3396857ffc599d09b030248790c194f8
Alex Fabiano <afabiano15@gmail.com>
2017-12-30 09:22:44 -0400

    adicionando opção de visualizar equipes passadas

--- src/app/pages/daily/daily.component.ts
+++ src/app/pages/daily/daily.component.ts
@@ -10,8 +10,19 @@ import { Observable } from 'rxjs/Observable';
 })
 export class DailyComponent {
 
+  teamKeySelected = '';
+
+  set teamKey(value: any) {
+    this.teamKeySelected = value;
+    this.updateView();
+  }
+
+  get teamKey() {
+    return this.teamKeySelected;
+  }
+
   team: Observable<any>;
-  items: Observable<Element[]>;
+  teams: Combo[] = new Array<Combo>();
 
   displayedColumns = ['position', 'name', 'totalMedalsWon', 'initialMedals', 'finalMedals', 'startDate', 'endDate'];
   dataSource = new MatTableDataSource(new Array<Element>());
@@ -19,14 +30,38 @@ export class DailyComponent {
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private db: AngularFireDatabase) {
-    this.team = db.object('team_players_rh/-L1EyslRaoS3BSldCdPj').valueChanges();
-    this.items = db.list('team_players_rh/-L1EyslRaoS3BSldCdPj/players').valueChanges();
-    this.items.subscribe(list => {
-      this.dataSource = new MatTableDataSource(list);
+    db.list('teams').snapshotChanges().subscribe(list => {
+      // tslint:disable-next-line:forin
+      for (const i in list) {
+        const combo = {
+          key: list[i].key,
+          value: list[i].payload.val().name
+        };
+        this.teams.push(combo);
+      }
+      this.teamKeySelected = this.teams[this.teams.length - 1].key;
+      this.updateView();
+    });
+  }
+
+  updateView() {
+    this.team = this.db.object('team_players_rh/' + this.teamKeySelected).valueChanges();
+    this.team.subscribe(list => {
+      const players: Element[] = new Array<Element>();
+      // tslint:disable-next-line:forin
+      for (const i in list.players) {
+        players.push(list.players[i]);
+      }
+      this.dataSource = new MatTableDataSource(players);
       this.dataSource.sort = this.sort;
     });
   }
 
+}
+
+export interface Combo {
+  key: string;
+  value: string;
 }
 
 export interface Element {
