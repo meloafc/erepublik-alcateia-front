55f10948467167d5501fca2a70fc696b96548c40
Alex Melo <alex.melo@maptechnology.com.br>
2018-03-21 15:04:15 -0400

    criando navegação inicial entre paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  private menuLateral: any;
+
+  constructor(
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+    private elementRef: ElementRef
+  ) { }
+
+
+  ngAfterViewInit() {
+    this.menuLateral = this.elementRef.nativeElement.querySelector('.mdl-layout');
+    this.cdr.detectChanges();
+  }
+
+  navegarParaTimeAtivo() {
+    this.router.navigate(['/active-team']);
+    this.alternarStatusMenuLateral();
+  }
+
+  alternarStatusMenuLateral() {
+    this.menuLateral.MaterialLayout.toggleDrawer();
+  }
+
 
 }
